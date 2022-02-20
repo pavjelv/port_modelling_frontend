afd523dffa9c24b9ab580a2284dfa8ca2107d324
Pavel O. Vorobev <pavel.o.vorobev@netcracker.com>
2022-02-21 00:03:03 +0300

    add package-locks

--- main-app/e2e/src/app.e2e-spec.ts
+++ main-app/e2e/src/app.e2e-spec.ts
@@ -1,17 +1,17 @@
-import { browser, logging } from 'protractor';
-import { AppPage } from './app.po';
+import { browser, logging } from "protractor";
+import { AppPage } from "./app.po";
 
-describe('workspace-project App', () => {
+describe("workspace-project App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it("should display welcome message", async () => {
     await page.navigateTo();
     expect(await page.getTitleText()).toEqual(
-      'port-modelling-fe app is running!'
+      "port-modelling-fe app is running!"
     );
   });
 
