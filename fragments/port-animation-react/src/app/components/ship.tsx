057a33e78a27004bdc6d7ff3ced613eae819d6a3
Pavel O. Vorobev <pavel.o.vorobev@netcracker.com>
2022-02-22 14:15:50 +0300

    change ship representation

--- fragments/port-animation-react/src/app/components/ship.tsx
+++ fragments/port-animation-react/src/app/components/ship.tsx
@@ -27,31 +27,58 @@ const ShipImage = (props: CustomerAnimationDataModel) => {
       sceneFunc={(context, shape) => {
         context.beginPath();
         // низ судна
+        context._context.fillStyle = "#213243";
         context.moveTo(0, 200);
         context.lineTo(600, 200);
         context.lineTo(530, 400);
         context.lineTo(55, 400);
         context.lineTo(0, 200);
+        context.fill();
         // палуба
+        context.beginPath();
+        context._context.fillStyle = "#283749";
         context.rect(15, 125, 170, 75);
+        context.fill();
+        context.beginPath();
+        context._context.fillStyle = "#45596a";
         context.rect(70, 70, 100, 55);
+        context.fill();
         // трубы
+        context.beginPath();
+        context._context.fillStyle = "#223244";
         context.rect(90, 30, 15, 40);
         context.arc(97, 30, 7, 0, Math.PI, true);
         context.rect(125, 15, 15, 55);
         context.arc(132, 15, 7, 0, Math.PI, true);
+        context.fill();
         // груз
+        context.beginPath();
+        context._context.fillStyle = "#e93d33";
         context.rect(260, 100, 150, 100);
+        context.fill();
+        context.beginPath();
+        context._context.fillStyle = "#24a38a";
         context.rect(410, 100, 150, 100);
+        context.fill();
+        context.beginPath();
+        context._context.fillStyle = "#f4ab3e";
         context.rect(360, 10, 150, 90);
+        context.fill();
+        context.closePath();
+
+        context._context.fillStyle = "white";
+        context._context.font = "80px calibri";
+        context.fillText(props.name, 200, 320);
+
+        // if we fill context ourselves, we need to mark object boundaries
+        context.beginPath();
+        context.moveTo(0, 0);
+        context.rect(0, 0, 600, 400);
         context.closePath();
         // (!) Konva specific method, it is very important
         context.fillStrokeShape(shape);
       }}
-      fill="#00D2FF"
-      stroke="black"
       draggable
-      strokeWidth={1}
       onClick={click}
       scale={{x: 0.15, y: 0.15}}
       y={y}
