ea03288da745ba17860d78c1b1fef2e0e9ddb221
Pavel O. Vorobev <pavel.o.vorobev@netcracker.com>
2022-03-22 18:16:12 +0300

    calculate values description

--- main-app/src/app/dictionaries/system-type.dictionary.ts
+++ main-app/src/app/dictionaries/system-type.dictionary.ts
@@ -1,7 +1,7 @@
 import { SystemType } from "../model/theory/system-type";
 
 export const SystemTypeDictionary = new Map<SystemType, string>([
-    [SystemType.WITH_QUEUE, "Многоканальная СМО с ограниченной очередью"],
-    [SystemType.INFINITE_QUEUE, "Многоканальная СМО с бесконечной очередью"],
-    [SystemType.WITH_REJECT, "Многоканальная СМО с отказами"],
+    [SystemType.WITH_QUEUE, "port-modelling-fe.systemType.withQueue"],
+    [SystemType.INFINITE_QUEUE, "port-modelling-fe.systemType.infiniteQueue"],
+    [SystemType.WITH_REJECT, "port-modelling-fe.systemType.withReject"],
 ]);
