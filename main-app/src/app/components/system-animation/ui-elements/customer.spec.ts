2862cba33c4aa0761030fcde43a86223ef04de8d
Pavel O. Vorobev <pavel.o.vorobev@netcracker.com>
2022-05-07 14:16:17 +0300

    add more test cases

--- main-app/src/app/components/system-animation/ui-elements/customer.spec.ts
+++ main-app/src/app/components/system-animation/ui-elements/customer.spec.ts
@@ -4,7 +4,7 @@ import { ShipType } from "../../../model/customer-data.model";
 describe("Customer", () => {
     let customer: Customer;
     beforeEach(() => {
-        const fakeContext = jasmine.createSpyObj("CanvasRenderingContext2D", ["clearRect", "fillStyle", "beginPath", "moveTo", "lineTo", "fill", "fillRect", "fillText"]);
+        const fakeContext = jasmine.createSpyObj("CanvasRenderingContext2D", ["clearRect", "fillStyle", "beginPath", "moveTo", "lineTo", "fill", "fillRect", "fillText", "translate", "rotate"]);
         customer = new Customer(fakeContext, ShipType.CARGO_SHIP, "name", 10, 20);
     });
 
@@ -39,5 +39,31 @@ describe("Customer", () => {
         spyOn(customer as any, "clear");
         customer.leave(1);
         expect((customer as any).clear).toHaveBeenCalledTimes(0);
+    });
+
+    it("should not do anything if already served", () => {
+        customer.success(1);
+        spyOn(customer as any, "clear");
+        customer.success(1);
+        expect((customer as any).clear).toHaveBeenCalledTimes(0);
+    });
+
+    it("should animate unload with timeout", () => {
+        const fakeServer = jasmine.createSpyObj("Server", ["animateUnload"]);
+        jasmine.clock().install();
+        customer.serve(1, fakeServer);
+        jasmine.clock().tick(5000);
+        expect(fakeServer.animateUnload).toHaveBeenCalledTimes(1);
+        jasmine.clock().uninstall();
+    });
+
+    it("should animate unload if already serving", () => {
+        const fakeServer = jasmine.createSpyObj("Server", ["animateUnload"]);
+        jasmine.clock().install();
+        customer.serve(1, fakeServer);
+        jasmine.clock().tick(5000);
+        customer.serve(1, fakeServer);
+        expect(fakeServer.animateUnload).toHaveBeenCalledTimes(2);
+        jasmine.clock().uninstall();
     });
 });
